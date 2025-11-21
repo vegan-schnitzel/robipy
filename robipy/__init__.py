a2802d6714d0d47544919aa0523fb7bda678201a
vegan-schnitzel <a271126@levante4.lvt.dkrz.de>
2025-11-21 18:51:54 +0100

    added internal imports

--- robipy/__init__.py
+++ robipy/__init__.py
@@ -1,1 +1,10 @@
-# from . import plot
+"""
+robipy
+======
+
+My personal python package as a loose collection
+of bits and bobs I wrote in the last couple of years.
+"""
+
+# make modules available when importing package
+from robipy import plot, proc, test
