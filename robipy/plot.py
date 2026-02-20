47479c625433ccec07e88f6032b01ded980b497d
vegan-schnitzel <a271126@levante4.lvt.dkrz.de>
2026-02-20 18:05:14 +0100

    updated ticks formatter

--- robipy/plot.py
+++ robipy/plot.py
@@ -66,8 +66,8 @@ def months_as_letter(ax):
 class MidpointNormalize(mpl.colors.Normalize):
     """
     Choose data range while preserving midpoint in divergent colorbars.
-    Caution: Should be used together with ClippedAutoLocator or AutoMidpointLocator
-             to avoid inconsistent colorbar labels!
+    Caution: Should be used together with ClippedAutoLocator or MidpointAutoLocator
+             to avoid inconsistent colorbar tick labels!
 
     Creates a subclass of mpl.colors.Normalize based on:
     https://stackoverflow.com/a/50003503
@@ -99,14 +99,14 @@ class ClippedAutoLocator(mpl.ticker.MaxNLocator):
         return ticks[(ticks >= vmin) & (ticks <= vmax)]
 
 
-class MidpointAutoLocator(mpl.ticker.AutoLocator):
+class MidpointAutoLocator(mpl.ticker.MaxNLocator):
     """
     Tick locator that forces ticks at vmin, midpoint, vmax.
     Other ticks (if any) follow AutoLocator's logic.
     """
-    def __init__(self, midpoint=0):
+    def __init__(self, midpoint=0, nbins='auto', steps=[1, 2, 2.5, 5, 10]):
         self.midpoint = midpoint
-        super().__init__()
+        super().__init__(nbins=nbins, steps=steps)
 
     def tick_values(self, vmin, vmax):
         # get default clipped ticks
@@ -124,19 +124,56 @@ class MidpointAutoLocator(mpl.ticker.AutoLocator):
         return ticks
 
 
-class MidpointFormatter(mpl.ticker.FuncFormatter):
+class MidpointMixedFormatter(mpl.ticker.FuncFormatter):
     """
     Formatter for mixed decimal/integer ticks.
+    NOTE: At some point just manually change ticks with FixedLocator!
     """
-    def __init__(self, vmin, vmax, midpoint=0, digit=2):
+    def __init__(self, vmin, vmax, midpoint=0, decimals=2):
         self.vmin = vmin
         self.vmax = vmax
         self.midpoint = midpoint
-        self.digit = digit
+        self.decimals = decimals
         super().__init__(self._format)
 
     def _format(self, x, pos):
-        if abs(x - self.vmin) < 1e-6 or abs(x - self.vmax) < 1e-6:
-            return f"{np.round(x, self.digit)}"
+        if (abs(x - self.vmin) < 1e-6) or (abs(x - self.vmax)) < 1e-6 or (abs(x - self.midpoint) < 1e-6):
+            return f"{np.round(x, self.decimals)}"
         else:
-            return f"{round(x)}"
+            return f"{int(np.round(x))}"
+
+
+class EdgeAutoLocator(mpl.ticker.MaxNLocator):
+    """
+    Subclass of mpl.ticker.AutoLocator that strictly places ticks at vmin & vmax.
+    """
+    def __init__(self, nbins='auto', steps=[1, 2, 2.5, 5, 10]):
+        super().__init__(nbins=nbins, steps=steps)
+
+    def tick_values(self, vmin, vmax):
+        ticks = super().tick_values(vmin, vmax)
+        ticks = ticks[(ticks >= vmin) & (ticks <= vmax)]
+
+        # replace the edge ticks with exact vmin/vmax
+        ticks[0] = vmin
+        ticks[-1] = vmax
+
+        return ticks
+
+
+class EdgeMixedFormatter(mpl.ticker.FuncFormatter):
+    """
+    Formatter for mixed decimal/integer ticks.
+    """
+    def __init__(self, vmin, vmax, edge_decimals=2, other_decimals=0):
+        self.vmin = vmin
+        self.vmax = vmax
+        self.edge_decimals = edge_decimals
+        self.other_decimals = other_decimals
+        super().__init__(self._format)
+
+    def _format(self, x, pos):
+        # handle vmin/vmax (compare values directly)
+        if abs(x - self.vmin) < 1e-6 or abs(x - self.vmax) < 1e-6:
+            return f"{x:.{self.edge_decimals}f}"
+        return f"{x:.{self.other_decimals}f}"
